a9d623e8ca4d6ee7aab6a36aa5a017043ad86643
Vlad_shayzi1 <tecnomega@mail.ru>
2025-05-25 12:14:56 +0300

    add watermark

--- bot/utils/chart.py
+++ bot/utils/chart.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
 from bot.log.logging_ import logging_
 
 
@@ -27,4 +31,23 @@ class Chart:
                plt.title(name)
                plt.savefig(path)
                plt.close()
-          return path
+          return await self._chart_watermark(input_image=path)
+
+     
+     async def _chart_watermark(
+          self,
+          input_image: str
+     ) -> str:
+          photo = Image.open(input_image)
+          drawing = ImageDraw.Draw(photo)
+          
+          pos = (0, 0)
+          text = "@fastfy_bot"
+          
+          font = ImageFont.truetype("bot/utils/fonts/SFProText-Regular.ttf", 20)
+          drawing.text(pos, text, font=font)
+          photo.save(input_image)
+          
+          logging_.chart.info(f"WATERMARK FOR {input_image}")
+          
+          return input_image
